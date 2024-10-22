968e79e8b7f85ec0780dfe1ccac4e4ac32f5e8da
makaut-anime-club <139333062+makaut-anime-club@users.noreply.github.com>
2024-10-22 21:06:40 +0530

    Update Hero.jsx hero tiltle

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
     <div className='hero container'>
       <div className="hero-text">
         <h1>'AKIMATSURI' 2024'</h1>
-        <p>presented by 'THE ANIME CLUB'</p>
+        <p>presented by 'MAKAUT ANIME CLUB'</p>
         <a href="https://docs.google.com/forms/d/e/1FAIpQLScmkuIGGKVrCcuKt_QZW7MVXfqEdljNuyxg_ZEuRJVBrsNltA/viewform?usp=sf_link"> <button className='btn'>Book Your Seat Now</button></a>
       </div>
     </div>
